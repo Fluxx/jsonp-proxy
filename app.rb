3b67832d0013dca32cbaf0a9e66fc1e6e0d28c5d
Jeff Pollard <jeff.pollard@gmail.com>
2011-12-11 11:04:59 -0800

    Simplify the app a lot to just do a GET and that's it

--- app.rb
+++ app.rb
@@ -1,40 +1,16 @@
-require 'rubygems'
 require 'sinatra'
 require "uri"
 require 'net/http'
-require 'net/https'
 require 'json'
 
 get '/' do
-  headers = params['h'] ? JSON.parse(params['h']) : {}
-  url = URI.parse(params['u'])
-  
-  if params['m'] == 'get'
-    request = Net::HTTP::Get.new(url.path)
-  elsif params['m'] == 'post'
-    request = Net::HTTP::Post.new(url.path)
-  elsif params['m'] == 'put'
-    request = Net::HTTP::Put.new(url.path)
-  elsif params['m'] == 'delete'
-    request = Net::HTTP::Delete.new(url.path)
-  elsif params['m'] == 'head'
-    request = Net::HTTP::Head.new(url.path)
-  else
-    request = Net::HTTP::Get.new(url.path)
-  end
-  
-  headers.each do |key, value|
-    request.add_field(key, value)
-  end
-  
-  request.body = params['b'] if params['b']
-  
-  http = Net::HTTP.new(url.host, url.port)
-  http.use_ssl = true
-  response = http.start do |http|
-    http.request(request)
-  end
+  response = Net::HTTP.get_response(URI.parse(params.fetch('u')))
 
-  arg = "{ status: #{response.code}, headers: [#{response.each_name { }.to_json}], body: '#{response.body}' }"
-  json = "#{params['jsonp']}(#{arg});"
+  arg = {
+    status: response.code,
+    headers: response.to_hash,
+    body: response.body
+  }
+
+  params['jsonp'] + "(#{arg.to_json})"  
 end
