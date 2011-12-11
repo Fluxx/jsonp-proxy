95529a746d35cbd4ef229c934aef767de11e6f31
Jeff Pollard <jeff.pollard@gmail.com>
2011-12-11 12:16:22 -0800

    Handle no 'u' paramter with a 200OK

--- app.rb
+++ app.rb
@@ -4,7 +4,11 @@ require 'net/http'
 require 'json'
 
 get '/' do
-  response = Net::HTTP.get_response(URI.parse(params.fetch('u')))
+  url = params.fetch('u') do
+    halt 'Please specifty a "u" query parater for the URL.'
+  end
+
+  response = Net::HTTP.get_response(URI.parse(url))
 
   arg = {
     status: response.code,
